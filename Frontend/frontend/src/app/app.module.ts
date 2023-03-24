84b245a8ea199f273d548ee81791fafdd5711fac
Megan <mnl31120@ucmo.edu>
2023-03-23 23:44:10 -0500

    Frontend Overhaul (All three pages in one branch)

--- Frontend/frontend/src/app/app.module.ts
+++ Frontend/frontend/src/app/app.module.ts
@@ -7,17 +7,44 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
+import { PDP1Component } from './pdp1/pdp1.component';
+import { PDP2Component } from './pdp2/pdp2.component';
+import { PDP3Component } from './pdp3/pdp3.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    PDP3Component,
+    PDP1Component,
+    PDP2Component,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NoopAnimationsModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatTabsModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatGridListModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
