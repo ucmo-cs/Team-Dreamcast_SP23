9251ffe176752a9cecb4f37b4a2fefb947c014cd
Clara Cerda <cmc17300@ucmo.edu>
2023-04-14 12:55:21 -0500

    patch: stuff added before i realized my stupidity

---
+++ Frontend/frontend/src/environments/environment.dev.ts
@@ -0,0 +1,23 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+//TODO: Replace the xxxxx in the below url with the url created in API Gateway after deploying the backend via serverless.
+
+//4/5/23 added the invoke url provided by harris, though it will need to be re-deployed since i changed the iam portion of the serverless.yml file
+
+export const environment = {
+    state: 'development',
+    production: false,
+    apiUrl: 'https://5guuo81sr4.execute-api.us-east-2.amazonaws.com',
+    
+  }
+  
+  /*
+   * For easier debugging in development mode, you can import the following file
+   * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+   *
+   * This import should be commented out in production mode because it will have a negative impact
+   * on performance if an error is thrown.
+   */
+  import 'zone.js/plugins/zone-error'  // Included with Angular CLI.
---
+++ Frontend/frontend/src/environments/environment.local.ts
@@ -0,0 +1,10 @@
+  //TODO: Replace the xxxxx in the below url with the url created in API Gateway after deploying the backend via serverless.
+
+  //4/5/23 again, changed .prod to .local and added the localhost url postman provides
+
+  export const environment = {
+    state: 'local',
+    production: false,
+    apiUrl: 'http://localhost:3000',
+  }
+  
