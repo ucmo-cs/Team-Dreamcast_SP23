3ecb1352cf226d92595c073d2ced32feb29fdc86
asmo <122326089+cmy13410@users.noreply.github.com>
2023-04-26 17:22:49 -0500

    Attempted connections from frontend to backend

--- Frontend/frontend/src/app/api.service.ts
+++ Frontend/frontend/src/app/api.service.ts
@@ -11,7 +11,7 @@ export class ApiService {
   private baseUrl = environment.apiUrl;
   private employeeURL = this.baseUrl + "/employees";
   private selfAssessmentURL = this.baseUrl + "/self-assessment";
-
+  private personalDevelopmentURL = this.baseUrl + "/personal-development-plan";
 
 //this constructor is to call the httpClient
   constructor(private http: HttpClient) { }
@@ -24,7 +24,21 @@ export class ApiService {
     return this.http.get<any>(this.selfAssessmentURL);
   }
 
-  getASelfAssessments(assessmentId:string) {
-    return this.http.get<any>(this.selfAssessmentURL + "/" + assessmentId);
+  getASelfAssessment(employeeId:string, assessmentYear:string) {
+    return this.http.get<any>(this.selfAssessmentURL + "/" + employeeId + "/" + assessmentYear);
   }
+
+  ////////
+
+  deletePersonalDevelopment(personalDevelopmentId:string) {
+    return this.http.delete<any>(this.personalDevelopmentURL + "/" + personalDevelopmentId);
+  }
+
+  getAPersonalDevelopment(planId:string) {
+    return this.http.get<any>(this.selfAssessmentURL + "/" + planId);
+  }
+
+
+  globalUser = 'First McLast';
+  globalUserId = '72858bf0-df17-11ed-828b-ed6fee87d3fd';
 }
